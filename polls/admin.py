cc97fe525421ec404c6713092eaa185cf78263bd
beepscore <support@beepscore.com>
2014-12-29 18:17:23 -0800

    In polls/admin.py add class QuestionAdmin and use it to change default field order in admin console.

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from polls.models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    # list publication date, then question text
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
