cef2339c4740eecfce68bb075a12971dd090f8d5
beepscore <support@beepscore.com>
2014-12-30 22:53:47 -0800

    In polls/admin.py fieldset import and register Choice. Now admin page shows Choices.

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Choice, Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -11,3 +11,4 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
