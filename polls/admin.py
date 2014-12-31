22c6cc6fee92fcb186a1e1428818dce82cb9f951
beepscore <support@beepscore.com>
2014-12-30 23:22:53 -0800

    In polls/admin.py delete register Choice, add class ChoiceInline.

--- polls/admin.py
+++ polls/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from polls.models import Choice, Question
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     # use fieldsets to show title
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
