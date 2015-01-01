d398de57364722987d7538f74bca32fb8287dda2
beepscore <support@beepscore.com>
2015-01-01 11:57:45 -0800

    In polls/models.py method was_published_recently assign attributes. In polls/admin.py add list_filter.

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,12 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date > (timezone.now() - datetime.timedelta(days=1))
 
+    # assign attribute admin_order_field to method was_published_recently
+    # http://blog.lerner.co.il/python-attributes/
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
